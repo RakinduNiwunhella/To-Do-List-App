92a741b21bbdea82c30390eaa124e3653140196e
Rakindu Niwunhella <rakindu15@gmail.com>
2025-08-16 16:06:35 +0530

    ji

--- To-Do-List-App/src/Components/Navbar/Navbar.jsx
+++ To-Do-List-App/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import './Navbar.css';
+import userProfile from './images/userProfile.svg';
+import logo from './images/logo.svg';
 
 const Navbar = () => {
     return (
-        <nav>
-        <h1>Task Master AI</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        </nav>
+        <header className="header">
+            <h1 className="header_title">
+                <img src={logo} alt="Logo" className="header_logo" /> Task Master AI
+            </h1>
+         
+            <div className="header_user">
+                <img src={userProfile} alt="User Profile" className="header_user-icon" />
+            </div>
+        </header>
     );
     }
 
